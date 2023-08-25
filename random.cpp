ec2add91efc0c13782ee53c1365eaa63fb60776d
Romain Simon <romainljsimon@gmail.com>
2023-08-25 10:06:14 +0200

    change random.cpp for more clarity + addition of print of seed

--- random.cpp
+++ random.cpp
@@ -6,7 +6,7 @@
  */
 #include <random>
 #include <iostream>
-
+#include "random.h"
 
 
 double randomDoubleGenerator(const double& min, const double& max)
@@ -15,7 +15,11 @@ double randomDoubleGenerator(const double& min, const double& max)
 	// For Fixed seed comment these two next lines and un comment the 3d !!
     static std::random_device rd;									// Used to obtain the seed for the following random number generator
 	static std::mt19937 mt(rd());
-    //static std::mt19937 mt;
+    static bool once = []()
+            {
+        std::cout << "double PRNG seed: " << std::to_string(rd()) << "\n";
+        return true;
+    } ();
     std::uniform_real_distribution<double> dist(min, max);          // The period of this generator is 2^9337-1. This there shouldn
     return dist(mt);												// This allows to transform the unsigned int mt into a random double between [min, max).
 }
@@ -23,8 +27,17 @@ double randomDoubleGenerator(const double& min, const double& max)
 int randomIntGenerator(const int& min, const int& max)
 {
 	// This function returns a random int from the {min, min + 1, ... , max  - 1, max} set.
-	return randomDoubleGenerator(static_cast<int>(min), static_cast<int>(max) + 1);
+    static std::random_device rd;									// Used to obtain the seed for the following random number generator
+    static std::mt19937 mt(rd());
+    static bool once = []()
+    {
+        std::cout << "int PRNG seed: " << std::to_string(rd()) << "\n";
+        return true;
+    } ();
+    std::uniform_int_distribution<int> dist(min, max);          // The period of this generator is 2^9337-1. This there shouldn
+    return dist(mt);
 }
+
 
 std::vector<double> randomVectorDoubleGenerator(const int& vectorSize, const double& min, const double& max)
 {
